337208b1380e31985919c50f1f4876a5fd7f7d98
YoujingYu99 <yuyoujing@hotmail.com>
2022-11-04 16:37:13 +0000

    More debugging into the original code.

--- src/facemesh.mjs
+++ src/facemesh.mjs
@@ -4,14 +4,14 @@ import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detec
  * Constructor of TFFaceMesh object
  * @constructor
  * */
-const TFFaceMesh = function() {
-  //Backend options are webgl, wasm, and CPU.
-  //For recent laptops WASM is better than WebGL.
-  this.model = faceLandmarksDetection.load(
-    faceLandmarksDetection.SupportedPackages.mediapipeFacemesh,
-    { maxFaces: 1 }
-  );
-  this.predictionReady = false;
+const TFFaceMesh = function () {
+    //Backend options are webgl, wasm, and CPU.
+    //For recent laptops WASM is better than WebGL.
+    this.model = faceLandmarksDetection.load(
+        faceLandmarksDetection.SupportedPackages.mediapipeFacemesh,
+        {maxFaces: 1}
+    );
+    this.predictionReady = false;
 };
 
 // Global variable for face landmark positions array
@@ -25,113 +25,116 @@ TFFaceMesh.prototype.positionsArray = null;
  * @param  {Number} height - of imageCanvas
  * @return {Object} the two eye-patches, first left, then right eye
  */
-TFFaceMesh.prototype.getEyePatches = async function(video, imageCanvas, width, height) {
+TFFaceMesh.prototype.getEyePatches = async function (video, imageCanvas, width, height) {
 
-  if (imageCanvas.width === 0) {
-    return null;
-  }
-
-  // Load the MediaPipe facemesh model.
-  const model = await this.model;
-
-  // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain an
-  // array of detected faces from the MediaPipe graph.
-  const predictions = await model.estimateFaces({
-    input: video,
-    returnTensors: false,
-    flipHorizontal: false,
-    predictIrises: false,
-  });
-
-  if (predictions.length === 0){
-    return false;
-  }
-
-  // Save positions to global variable
-  this.positionsArray = predictions[0].scaledMesh;
-  const positions = this.positionsArray;
-
-  const { scaledMesh } = predictions[0];
-  // Keypoints indexes are documented at
-  // https://github.com/tensorflow/tfjs-models/blob/118d4727197d4a21e2d4691e134a7bc30d90deee/face-landmarks-detection/mesh_map.jpg
-  const [leftBBox, rightBBox] = [
-    // left
-    {
-      eyeTopArcKeypoints: [
-        25, 33, 246, 161, 160, 159, 158, 157, 173, 243,
-      ],
-      eyeBottomArcKeypoints: [
-        25, 110, 24, 23, 22, 26, 112, 243,
-      ],
-    },
-    // right
-    {
-      eyeTopArcKeypoints: [
-        463, 398, 384, 385, 386, 387, 388, 466, 263, 255,
-      ],
-      eyeBottomArcKeypoints: [
-        463, 341, 256, 252, 253, 254, 339, 255,
-      ],
-    },
-  ].map(({ eyeTopArcKeypoints, eyeBottomArcKeypoints }) => {
-    const topLeftOrigin = {
-      x: Math.round(Math.min(...eyeTopArcKeypoints.map(k => scaledMesh[k][0]))),
-      y: Math.round(Math.min(...eyeTopArcKeypoints.map(k => scaledMesh[k][1]))),
-    };
-    const bottomRightOrigin = {
-      x: Math.round(Math.max(...eyeBottomArcKeypoints.map(k => scaledMesh[k][0]))),
-      y: Math.round(Math.max(...eyeBottomArcKeypoints.map(k => scaledMesh[k][1]))),
-    };
-
-    return {
-      origin: topLeftOrigin,
-      width: bottomRightOrigin.x - topLeftOrigin.x,
-      height: bottomRightOrigin.y - topLeftOrigin.y,
+    if (imageCanvas.width === 0) {
+        return null;
     }
-  });
-  var leftOriginX = leftBBox.origin.x;
-  var leftOriginY = leftBBox.origin.y;
-  var leftWidth = leftBBox.width;
-  var leftHeight = leftBBox.height;
-  var rightOriginX = rightBBox.origin.x;
-  var rightOriginY = rightBBox.origin.y;
-  var rightWidth = rightBBox.width;
-  var rightHeight = rightBBox.height;
 
-  if (leftWidth === 0 || rightWidth === 0){
-    console.log('an eye patch had zero width');
-    return null;
-  }
+    // Load the MediaPipe facemesh model.
+    const model = await this.model;
 
-  if (leftHeight === 0 || rightHeight === 0){
-    console.log('an eye patch had zero height');
-    return null;
-  }
+    // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain an
+    // array of detected faces from the MediaPipe graph.
+    const predictions = await model.estimateFaces({
+        input: video,
+        returnTensors: false,
+        flipHorizontal: false,
+        predictIrises: false,
+    });
 
-  // Start building object to be returned
-  var eyeObjs = {};
+    if (predictions.length === 0) {
+        return false;
+    }
 
-  var leftImageData = imageCanvas.getContext('2d').getImageData(leftOriginX, leftOriginY, leftWidth, leftHeight);
-  eyeObjs.left = {
-    patch: leftImageData,
-    imagex: leftOriginX,
-    imagey: leftOriginY,
-    width: leftWidth,
-    height: leftHeight
-  };
+    // Save positions to global variable
+    this.positionsArray = predictions[0].scaledMesh;
+    const positions = this.positionsArray;
 
-  var rightImageData = imageCanvas.getContext('2d').getImageData(rightOriginX, rightOriginY, rightWidth, rightHeight);
-  eyeObjs.right = {
-    patch: rightImageData,
-    imagex: rightOriginX,
-    imagey: rightOriginY,
-    width: rightWidth,
-    height: rightHeight
-  };
+    const {scaledMesh} = predictions[0];
+    // Keypoints indexes are documented at
+    // https://github.com/tensorflow/tfjs-models/blob/118d4727197d4a21e2d4691e134a7bc30d90deee/face-landmarks-detection/mesh_map.jpg
+    const [leftBBox, rightBBox] = [
+        // left
+        {
+            eyeTopArcKeypoints: [
+                25, 33, 246, 161, 160, 159, 158, 157, 173, 243,
+            ],
+            eyeBottomArcKeypoints: [
+                25, 110, 24, 23, 22, 26, 112, 243,
+            ],
+        },
+        // right
+        {
+            eyeTopArcKeypoints: [
+                463, 398, 384, 385, 386, 387, 388, 466, 263, 255,
+            ],
+            eyeBottomArcKeypoints: [
+                463, 341, 256, 252, 253, 254, 339, 255,
+            ],
+        },
+    ].map(({eyeTopArcKeypoints, eyeBottomArcKeypoints}) => {
+        const topLeftOrigin = {
+            x: Math.round(Math.min(...eyeTopArcKeypoints.map(k => scaledMesh[k][0]))),
+            y: Math.round(Math.min(...eyeTopArcKeypoints.map(k => scaledMesh[k][1]))),
+        };
+        const bottomRightOrigin = {
+            x: Math.round(Math.max(...eyeBottomArcKeypoints.map(k => scaledMesh[k][0]))),
+            y: Math.round(Math.max(...eyeBottomArcKeypoints.map(k => scaledMesh[k][1]))),
+        };
 
-  this.predictionReady = true;
+        return {
+            origin: topLeftOrigin,
+            width: bottomRightOrigin.x - topLeftOrigin.x,
+            height: bottomRightOrigin.y - topLeftOrigin.y,
+        }
+    });
+    var leftOriginX = leftBBox.origin.x;
+    var leftOriginY = leftBBox.origin.y;
+    var leftWidth = leftBBox.width;
+    var leftHeight = leftBBox.height;
+    var rightOriginX = rightBBox.origin.x;
+    var rightOriginY = rightBBox.origin.y;
+    var rightWidth = rightBBox.width;
+    var rightHeight = rightBBox.height;
 
-  return eyeObjs;
+    if (leftWidth === 0 || rightWidth === 0) {
+        console.log('an eye patch had zero width');
+        return null;
+    }
+
+    if (leftHeight === 0 || rightHeight === 0) {
+        console.log('an eye patch had zero height');
+        return null;
+    }
+
+    // Start building object to be returned
+    var eyeObjs = {};
+
+    var leftImageData = imageCanvas.getContext('2d').getImageData(leftOriginX, leftOriginY, leftWidth, leftHeight);
+    eyeObjs.left = {
+        patch: leftImageData,
+        imagex: leftOriginX,
+        imagey: leftOriginY,
+        width: leftWidth,
+        height: leftHeight
+    };
+
+    var rightImageData = imageCanvas.getContext('2d').getImageData(rightOriginX, rightOriginY, rightWidth, rightHeight);
+    eyeObjs.right = {
+        patch: rightImageData,
+        imagex: rightOriginX,
+        imagey: rightOriginY,
+        width: rightWidth,
+        height: rightHeight
+    };
+
+    eyeObjs = pupil.getPupils(eyeObjs);
+    console.log(eyeObjs)
+
+    this.predictionReady = true;
+
+    return eyeObjs;
 };
 
 /**
@@ -139,36 +142,36 @@ TFFaceMesh.prototype.getEyePatches = async function(video, imageCanvas, width, h
  * Requires that getEyePatches() was called previously, else returns null.
  */
 TFFaceMesh.prototype.getPositions = function () {
-  return this.positionsArray;
+    return this.positionsArray;
 }
 
 /**
  * Reset the tracker to default values
  */
-TFFaceMesh.prototype.reset = function(){
-  console.log( "Unimplemented; Tracking.js has no obvious reset function" );
+TFFaceMesh.prototype.reset = function () {
+    console.log("Unimplemented; Tracking.js has no obvious reset function");
 }
 
 /**
  * Draw TF_FaceMesh_Overlay
  */
-TFFaceMesh.prototype.drawFaceOverlay = function(ctx, keypoints){
-  // If keypoints is falsy, don't do anything
-  if (keypoints) {
-    ctx.fillStyle = '#32EEDB';
-    ctx.strokeStyle = '#32EEDB';
-    ctx.lineWidth = 0.5;
+TFFaceMesh.prototype.drawFaceOverlay = function (ctx, keypoints) {
+    // If keypoints is falsy, don't do anything
+    if (keypoints) {
+        ctx.fillStyle = '#32EEDB';
+        ctx.strokeStyle = '#32EEDB';
+        ctx.lineWidth = 0.5;
 
-    for (let i = 0; i < keypoints.length; i++) {
-      const x = keypoints[i][0];
-      const y = keypoints[i][1];
+        for (let i = 0; i < keypoints.length; i++) {
+            const x = keypoints[i][0];
+            const y = keypoints[i][1];
 
-      ctx.beginPath();
-      ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
-      ctx.closePath();
-      ctx.fill();
+            ctx.beginPath();
+            ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
+            ctx.closePath();
+            ctx.fill();
+        }
     }
-  }
 }
 
 /**
